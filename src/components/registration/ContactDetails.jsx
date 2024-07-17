db7694e86bdfce44daf6c6f87567669d7ca563f7
ramachandrapetla <ramachandra1996@gmail.com>
2024-07-17 16:52:33 -0700

    connect contactinfo to store

--- src/components/registration/ContactDetails.jsx
+++ src/components/registration/ContactDetails.jsx
@@ -1,18 +1,41 @@
-import { useState } from "react";
 import FormField from "../storybook/FormField";
+import { connect } from "react-redux";
+import { setContactInfoByFieldName } from "../../app/features/registration/contactinfo.slice";
 
-const ContactDetails = () => {
-    const [contactInfo, setContactInfo] = useState({
-        email: "",
-        phone: ""
-    })
+const ContactDetails = ({contactInfo, dispatch}) => {
+    
+    const handleChange= (e) => {
+        switch(e.target.name) {
+            case "email": 
+                dispatch(setContactInfoByFieldName({
+                    name: "email",
+                    value: e.target.value
+                }))
+                break;
+            case "phone": 
+                dispatch(setContactInfoByFieldName({
+                    name: "phone",
+                    value: e.target.value
+                }))
+                break;
+        }
+    }
 
     return (
-        <div> 
-            <FormField placeholder="eg. youremail@domain.com" label="Enter email address" required={true}/>
-            <FormField placeholder="eg. 456-343-9843" label="Enter phone number" required={true}/>
+        <div onChange={handleChange}> 
+            <FormField placeholder="eg. youremail@domain.com" label="Enter email address" type="text" value={contactInfo.email}required={true} name="email"/>
+            <FormField placeholder="eg. 456-343-9843" label="Enter phone number" type="tel" value={contactInfo.phone} required={true} name="phone"/>
         </div>
     )
 }
 
-export default ContactDetails;
+function mapStateToProps(state) {
+    
+    const {contactInfo} = state;
+
+    return {
+        contactInfo
+    }
+}
+
+export default connect(mapStateToProps)(ContactDetails)
