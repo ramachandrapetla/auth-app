6f1bd7d5979982b58788c38d5cc1b2fa3ff3e185
ramachandrapetla <ramachandra1996@gmail.com>
2024-07-20 14:40:54 -0700

    added validations for personal Info

--- src/components/registration/PersonalInformation.jsx
+++ src/components/registration/PersonalInformation.jsx
@@ -2,30 +2,39 @@ import { useEffect, useState } from "react";
 import FormField from "../storybook/FormField";
 import { connect } from "react-redux";
 import { setPersonalInfoByFieldName } from "../../app/features/registration/personalinfo.slice";
+import { getMaxEligibleDob, getMinEligibleDob, getTodayDate, validatePersonalInfo } from "../../utility/registration.validation";
 
 const PersonalInformation = ({personalInfo, handleNext, dispatch}) => {
 
+    const [nextDisabled, setNextDisabled] = useState(!validatePersonalInfo(personalInfo))
+    const updatedInfo = {...personalInfo}
     const handleChange= (e) => {
         switch(e.target.name) {
             case "firstName": 
+                updatedInfo.firstName = e.target.value
                 dispatch(setPersonalInfoByFieldName({
                     name: "firstName",
                     value: e.target.value
                 }))
                 break;
             case "lastName": 
+                updatedInfo.lastName = e.target.value
                 dispatch(setPersonalInfoByFieldName({
                     name: "lastName",
                     value: e.target.value
                 }))
                 break;
-            case "dob": 
+            case "dob":
+                updatedInfo.dob = e.target.value 
                 dispatch(setPersonalInfoByFieldName({
                     name: "dob",
                     value: e.target.value
                 }))
                 break;
         }
+        const isValid = validatePersonalInfo(updatedInfo)
+        console.log("Valid: ", isValid)
+        setNextDisabled(!isValid)
     }
 
     return (
@@ -33,10 +42,10 @@ const PersonalInformation = ({personalInfo, handleNext, dispatch}) => {
             <div onChange={handleChange}> 
                 <FormField placeholder="John" label="Enter First Name" required={true} value={personalInfo.firstName} name="firstName"/>
                 <FormField placeholder="Smith" label="Enter Last Name" required={true} value={personalInfo.lastName} name="lastName"/>
-                <FormField label="Enter Date of Birth" required={true} value={personalInfo.dob} name="dob" type="date"/>
+                <FormField label="Enter Date of Birth" required={true} value={personalInfo.dob} name="dob" type="date" min={getMinEligibleDob()} max={getMaxEligibleDob()}/>
             </div>
             <div className="button-container">
-                <button onClick={handleNext}>Next</button>
+                <button onClick={handleNext} disabled={nextDisabled}>Next</button>
             </div>
         </div>
     )
--- src/utility/registration.validation.js
+++ src/utility/registration.validation.js
@@ -1,3 +1,4 @@
+//Contact details validation
 export const validateEmail = (email, setError, displayError=true) => {
     if(displayError) setError((state) => ({...state, emailError: null}))
     const emailRegexPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
@@ -24,7 +25,6 @@ export const validateEmail = (email, setError, displayError=true) => {
             emailError: validationResult.errorMessage
         }))
     }
-    console.log(validationResult)
     if(validationResult.valid && setError) setError((state) => ({...state, emailError: null}))
     return validationResult
 }
@@ -60,12 +60,6 @@ export const validatePhoneNumber = (phone, setError, displayError=true) => {
     return validationResult
 }
 
-export const validateContactDetails = (contactInfo, setError) => {
-    let valid = true;
-    if(!validatePhoneNumber(contactInfo.phone.formatted, setError, false).valid) valid=false
-    if(!validateEmail(contactInfo.email, setError, false).valid) valid=false
-    return valid
-}
 
 export const formatPhoneNumber = (number) => {
     number = number.replace("-", "");
@@ -79,3 +73,100 @@ export const formatPhoneNumber = (number) => {
         return `(${number.slice(0, 3)}) ${number.slice(3, 6)}-${number.slice(6, 10)}`;
     }
 }
+
+export const validateContactDetails = (contactInfo, setError) => {
+    let valid = true;
+    if(!validatePhoneNumber(contactInfo.phone.formatted, setError, false).valid) valid=false
+    if(!validateEmail(contactInfo.email, setError, false).valid) valid=false
+    return valid
+}
+
+// Personal Details Validation
+export const validateName = (field, value) => {
+    const validationResult = {
+        valid: false,
+        errorMessage: ""
+    }
+    if(value === "") { 
+        validationResult.errorMessage = field + "is required" }
+    else {
+        validationResult.valid = true
+    }
+    console.log("Name: ", field, value, validationResult.valid)
+    return validationResult
+}
+
+export const validateDob = (dob) => {
+    const validationResult = {
+        valid: false,
+        errorMessage: ""
+    }
+
+    if(dob === "") {
+        validationResult.errorMessage = "date of birth is required"
+    } else {
+        const [year, month, day] = dob.split('-').map(Number);
+        const currentDate = new Date();
+        const inputDate = new Date(year, month - 1, day);
+
+        const age = currentDate.getFullYear() - inputDate.getFullYear();
+        if(age >= 16 && age < 100) {
+            validationResult.valid = true
+        } else {
+            validationResult.errorMessage = "Age should be between 16 and 100"
+        }
+    }
+    console.log("Validate DOB: ", validationResult.valid, validationResult.errorMessage)
+    return validationResult
+}
+
+export const getTodayDate = () => {
+    const today = new Date();
+
+    // Get year, month, and day as strings with leading zeros if necessary
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Month is zero-indexed
+    const day = String(today.getDate()).padStart(2, '0');
+
+    // Format the date as yyyy-mm-dd
+    const formattedDate = `${year}-${month}-${day}`;
+
+    return formattedDate;
+}
+
+export const getMaxEligibleDob = () => {
+    const today = new Date();
+
+    // Get year, month, and day as strings with leading zeros if necessary
+    const year = today.getFullYear() - 16;
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Month is zero-indexed
+    const day = String(today.getDate()).padStart(2, '0');
+
+    // Format the date as yyyy-mm-dd
+    const formattedDate = `${year}-${month}-${day}`;
+
+    return formattedDate;
+}
+
+export const getMinEligibleDob = () => {
+    const today = new Date();
+
+    // Get year, month, and day as strings with leading zeros if necessary
+    const year = today.getFullYear() - 100;
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Month is zero-indexed
+    const day = String(today.getDate()).padStart(2, '0');
+
+    // Format the date as yyyy-mm-dd
+    const formattedDate = `${year}-${month}-${day}`;
+
+    return formattedDate;
+}
+
+export const validatePersonalInfo = (personalInfo) => {
+    let isValid = true
+    if(!validateName("first name", personalInfo.firstName).valid) isValid = false
+    if(!validateName("last name", personalInfo.lastName).valid) isValid = false
+    if(!validateDob(personalInfo.dob).valid) isValid = false
+    return isValid
+}
+
